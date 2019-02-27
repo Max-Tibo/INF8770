3834fa863dbb7af0a355b6d963edbc653a3ab233
scouture123 <samuelcouture@hotmail.com>
2019-02-27 17:54:21 -0500

    finition étape 1

--- INF8770-TP2/TP2_INF8770/Program.cs
+++ INF8770-TP2/TP2_INF8770/Program.cs
@@ -19,93 +19,70 @@ namespace TP2_INF8770
 
         public static Bitmap rgb2ycbcr(Bitmap bmp)
         {
-
             int width = bmp.Width;
             int height = bmp.Height;
+            Bitmap newBitmap = new Bitmap(bmp.Width, bmp.Height);
             byte[,] yData = new byte[width, height];                   
             byte[,] bData = new byte[width, height];                   
             byte[,] rData = new byte[width, height];                   
 
-            unsafe
+            //Convert to YCbCr
+            for (int y = 0; y < height; y++)
             {
-                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bmp.PixelFormat);
-                int heightInPixels = bitmapData.Height;
-                int widthInBytes = width * 3;
-                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
-                Bitmap newBitmap = new Bitmap(bmp.Width, bmp.Height);
-                //Convert to YCbCr
-                for (int y = 0; y < heightInPixels; y++)
+                for (int x = 0; x < width; x++)
                 {
-                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
-                    for (int x = 0; x < width; x++)
-                    {
-                        int xPor3 = x*4;
-                        float blue = currentLine[xPor3++];
-                        float green = currentLine[xPor3++];
-                        float red = currentLine[xPor3];
+                    float blue = bmp.GetPixel(x, y).B;
+                    float green = bmp.GetPixel(x, y).G; 
+                    float red = bmp.GetPixel(x, y).R;
 
-                        double Y = (0.299 * red) + (0.587 * green) + (0.114 * blue);
-                        double Cb = 128 + 0.564 * (blue - Y);
-                        double Cr = 128 + 0.713 * (red - Y);
+                    double Y = (0.299 * red) + (0.587 * green) + (0.114 * blue);
+                    double Cb = 128 + 0.564 * (blue - Y);
+                    double Cr = 128 + 0.713 * (red - Y);
 
-                        yData[x, y] = (byte)Y;
-                        bData[x, y] = (byte)Cb;
-                        rData[x, y] = (byte)Cr;
+                    yData[x, y] = (byte)Y;
+                    bData[x, y] = (byte)Cb;
+                    rData[x, y] = (byte)Cr;
 
-                        Color nouvelleCouleur = Color.FromArgb(yData[x, y], bData[x, y], rData[x, y]);
-                        newBitmap.SetPixel(x, y, nouvelleCouleur);
-                    }
+                    Color nouvelleCouleur = Color.FromArgb(yData[x, y], bData[x, y], rData[x, y]);
+                    newBitmap.SetPixel(x, y, nouvelleCouleur);
                 }
-                bmp.UnlockBits(bitmapData);
-                newBitmap.Save("mybmp.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
-                return newBitmap;
             }
+            newBitmap.Save("mybmp.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            return newBitmap;
         }
 
         public static Bitmap ycbcr2rgb(Bitmap bmp)
         {
-
             int width = bmp.Width;
             int height = bmp.Height;
-            byte[,] rData = new byte[width, height];                   
-            byte[,] gData = new byte[width, height];                   
-            byte[,] bData = new byte[width, height];                   
+            Bitmap newBitmap = new Bitmap(bmp.Width, bmp.Height);
+            byte[,] rData = new byte[width, height];
+            byte[,] gData = new byte[width, height];
+            byte[,] bData = new byte[width, height];
 
-            unsafe
+            //Convert to RGB
+            for (int y = 0; y < height; y++)
             {
-                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bmp.PixelFormat);
-                int heightInPixels = bitmapData.Height;
-                int widthInBytes = width * 3;
-                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
-                Bitmap newBitmap = new Bitmap(bmp.Width, bmp.Height);
-                //Convert to YCbCr
-                for (int y = 0; y < heightInPixels; y++)
+                for (int x = 0; x < width; x++)
                 {
-                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
-                    for (int x = 0; x < width; x++)
-                    {
-                        int xPor4 = x * 4;
-                        float Y = currentLine[xPor4++];
-                        float Cb = currentLine[xPor4++];
-                        float Cr = currentLine[xPor4];
+                    float Y = bmp.GetPixel(x, y).R;
+                    float Cb = bmp.GetPixel(x, y).G;
+                    float Cr = bmp.GetPixel(x, y).B;
 
+                    double red = Y + 1.403 * (Cr - 128);
+                    double green = Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128);
+                    double blue = Y + 1.773 * (Cb - 128);
 
-                        double red = Y + 1.403 * (Cr - 128);
-                        double green = Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128);
-                        double blue = Y + 1.773 * (Cb - 128);
+                    rData[x, y] = (byte)red;
+                    gData[x, y] = (byte)green;
+                    bData[x, y] = (byte)blue;
 
-                        rData[x, y] = (byte)red;
-                        gData[x, y] = (byte)green;
-                        bData[x, y] = (byte)blue;
-
-                        Color nouvelleCouleur = Color.FromArgb(rData[x, y], gData[x, y], bData[x, y]);
-                        newBitmap.SetPixel(x, y, nouvelleCouleur);
-                    }
+                    Color nouvelleCouleur = Color.FromArgb(rData[x, y], gData[x, y], bData[x, y]);
+                    newBitmap.SetPixel(x, y, nouvelleCouleur);
                 }
-                bmp.UnlockBits(bitmapData);
-                newBitmap.Save("retour.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
-                return newBitmap;
             }
+            newBitmap.Save("test.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            return newBitmap;
         }
 
         public void decoupage8x8(Bitmap bitmap)
