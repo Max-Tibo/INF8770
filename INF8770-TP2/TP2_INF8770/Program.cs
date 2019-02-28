e7c66ed4456bedadda755e3b9463883afa702b4f
MaxThi <maxime.thibault.371@outlook.com>
2019-02-28 00:16:05 -0500

    Ajout de huffman, rle, etc

--- INF8770-TP2/TP2_INF8770/Program.cs
+++ INF8770-TP2/TP2_INF8770/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Collections;
 
 namespace TP2_INF8770
 {
@@ -13,18 +14,42 @@ namespace TP2_INF8770
         static void Main(string[] args)
         {
             Bitmap imageBM = new Bitmap("mario.png");
-            Bitmap nouvelleimage = rgb2ycbcr(imageBM);
+            int width = imageBM.Width;
+            int height = imageBM.Height;
+
+            // Création des tableaux 2D qui vont contenir les informations de Y Cb Cr
+            byte[,] yData = new byte[width, height]; 
+            byte[,] bData = new byte[width/2, height/2];
+            byte[,] rData = new byte[width/2, height/2];
+
+            // Applique la conversion et division des résultats de chaques composantes dans leur tableau respectif
+            Bitmap nouvelleimage = rgb2ycbcr(imageBM, yData, bData, rData);
             ycbcr2rgb(nouvelleimage);
-            decoupage8x8(imageBM);
+
+            // Création des listes qui vont contenir tous les tableaux 8x8 de chaque élément Y Cb Cr
+            List<byte[,]> yBlocks = new List<byte[,]>(); 
+            List<byte[,]> bBlocks = new List<byte[,]>();
+            List<byte[,]> rBlocks = new List<byte[,]>();
+
+            // Remplissage des listes
+            yBlocks = decoupage8x8(yData);
+            bBlocks = decoupage8x8(bData);
+            rBlocks = decoupage8x8(rData);
+
+            // Merge toutes les listes de blocs 8x8 ensemble en vue de la dct
+            List<byte[,]> blocks = new List<byte[,]>();
+            blocks.AddRange(yBlocks);
+            blocks.AddRange(bBlocks);
+            blocks.AddRange(rBlocks);
+
+            // Applique la dct
+            DCT(blocks);
         }
 
         public static Bitmap rgb2ycbcr(Bitmap bmp, byte[,] yData, byte[,] bData, byte[,] rData)
         {
             int width = bmp.Width;
-            int height = bmp.Height;
-            yData = new byte[width, height];                   
-            bData = new byte[width/2, height/2];                   
-            rData = new byte[width/2, height/2];                   
+            int height = bmp.Height;                   
 
             //Convert to YCbCr
             for (int y = 0; y < height; y++)
@@ -117,7 +142,7 @@ namespace TP2_INF8770
             return tab8x8;
         }
 
-        public void DCT(List<Color[,]> tab8x8)
+        public void DCT(List<byte[,]> tab8x8)
         {
             int n = 8;
             double w, z;
@@ -149,6 +174,234 @@ namespace TP2_INF8770
                     //créer nouveau bloc 8x8 avec les fréquences obtenu
                     //oOutput(u - 1, v - 1) = dct_transform;
                 }
+            }
+        }
+    }
+
+    // Ajout des classes et méthodes nécessaire pour la compression de huffman:
+    // https://www.csharpstar.com/csharp-huffman-coding-using-dictionary/
+    public class Node
+    {
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+ 
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            // Leaf
+            if (Right == null && Left == null)
+            {
+                if (symbol.Equals(this.Symbol))
+                {
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+ 
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+ 
+                    left = Left.Traverse(symbol, leftPath);
+                }
+ 
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(symbol, rightPath);
+                }
+ 
+                if (left != null)
+                {
+                    return left;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+
+    public class HuffmanTree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+ 
+        public void Build(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+ 
+                Frequencies[source[i]]++;
+            }
+ 
+            foreach (KeyValuePair<char, int> symbol in Frequencies)
+            {
+                nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+ 
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+ 
+                if (orderedNodes.Count >= 2)
+                {
+                    // Take first two items
+                    List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+ 
+                    // Create a parent node by combining the frequencies
+                    Node parent = new Node()
+                    {
+                        Symbol = '*',
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        Left = taken[0],
+                        Right = taken[1]
+                    };
+ 
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+ 
+                this.Root = nodes.FirstOrDefault();
+ 
+            }
+ 
+        }
+ 
+        public BitArray Encode(string source)
+        {
+            List<bool> encodedSource = new List<bool>();
+ 
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+ 
+            BitArray bits = new BitArray(encodedSource.ToArray());
+ 
+            return bits;
+        }
+ 
+        public string Decode(BitArray bits)
+        {
+            Node current = this.Root;
+            string decoded = "";
+ 
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                    }
+                }
+ 
+                if (IsLeaf(current))
+                {
+                    decoded += current.Symbol;
+                    current = this.Root;
+                }
+            }
+ 
+            return decoded;
+        }
+ 
+        public bool IsLeaf(Node node)
+        {
+            return (node.Left == null && node.Right == null);
+        }
+ 
+    }
+
+    // Ajout des classes et méthodes nécessaire pour la compression RLE:
+    // https://gist.github.com/lsauer/3744846 && http://en.wikipedia.org/wiki/Run-length_encoding
+    public class Transform 
+    {
+        public const char EOF = '\u007F';
+        public const char ESCAPE = '\\';
+
+        public static string RunLengthEncode(string s)
+        {
+            try
+            {
+                string srle = string.Empty;
+                int ccnt = 1; //char counter
+                for (int i = 0; i < s.Length-1; i++ )
+                {
+                    if (s[i] != s[i + 1] || i == s.Length-2 ) //..a break in character repetition or the end of the string
+                    {
+                        if (s[i] == s[i + 1] && i == s.Length - 2) //end of string condition
+                            ccnt++;
+                        srle += ccnt + ("1234567890".Contains(s[i]) ? ""+ESCAPE : "") + s[i]; //escape digits
+                        if (s[i] != s[i + 1] && i == s.Length - 2) //end of string condition
+                            srle += ("1234567890".Contains(s[i + 1]) ? "1" + ESCAPE : "") + s[i + 1]; 
+                        ccnt = 1; //reset char repetition counter
+                    }
+                    else {
+                        ccnt++;
+                    }
+
+                }
+                return srle;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in RLE:" + e.Message);
+                return null;
+            }
+        }
+        public static string RunLengthDecode(string s)
+        {
+            try {
+                string  dsrle = string.Empty
+                        , ccnt = string.Empty; //char counter
+                for (int i = 0; i < s.Length; i++) {
+                    if ( "1234567890".Contains(s[i]) ) //extract repetition counter
+                    {
+                        ccnt += s[i];
+                    }
+                    else {
+                        if (s[i] == ESCAPE) {
+                            i++;
+                        }
+                        dsrle += new String(s[i], int.Parse(ccnt));
+                        ccnt = "";
+                    }
+
+                }
+                return dsrle;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception in RLD:" + e.Message);
+                return null;
             }
         }
     }
